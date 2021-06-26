74c148dfa76fdb985d732504fa3aaaacce2b5f35
Jay <jianjunjchen@gmail.com>
2021-06-26 17:36:31 +1000

    auth register/login/logout

--- backend/auth.py
+++ backend/auth.py
@@ -2,29 +2,32 @@ import string
 import random
 import jwt
 import re
+import hashlib
 
 VALID_EMAIL = r"^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$"
-def generate_token(u_id):
-    """
-    return a new token for authenticaiton in users' session
-    Input:
-    - uid (integer)
 
-    Output:
-    - token(string)
-    """
-    # generate SECRET by using radom string--> len(random String) == 22
+# function for generating a token by given uid
+def generate_token(u_id):
+    # generate SECRET by using radom string--> len(random String) == 25
     letters = string.ascii_letters
-    secret = ''.join(random.choice(letters) for i in range(22))
+    secret = ''.join(random.choice(letters) for i in range(25))
     token = jwt.encode({'user_id': u_id}, secret, algorithm='HS256')
     return token
 
-def diner_register(server, email, password, first_name, last_name, phone)
+# function for hashing a password to protect the secuity
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+# function for checking if the email is used (1)
+def if_email_exist(email):
+    return False
+
+def diner_register(email, password, first_name, last_name, phone):
     # check if the email is valid
     if not re.search(VALID_EMAIL, email):
         raise InputError("Email invalid")
     # check if the email addess is being used by another user
-    if server.if_email.exist(email):
+    if if_email_exist(email):
         raise InputError("Email being used")
     # check if the password entered is less than 6 character or empty
     if len(password) < 6:
@@ -39,9 +42,52 @@ def diner_register(server, email, password, first_name, last_name, phone)
         raise InputError("First name invalid")
     if len(last_name) > 50:
         raise InputError("Last name invalid")
-    
-    
 
+    # hash the password for security
+    hashed_password = hash_password(password)
+    
+    # generate user_id and token, uid = 500000+user_number
+    u_id = 500000
+    token = generate_token(u_id)
+    
+    # create an eatery and store in the database (2) !!!!
+    store_eatery(u_id, email, hashed_password, first_name, last_name, phone)
+
+    return {'u_id': u_id, 'token': token}
+
+# login function
+# given a registered user's email and password
+# generates a valid token for the user to remain authenticated
+def auth_login(email, passowrd):
+    # check if the email is valid
+    if not re.search(VALID_EMAIL, email):
+        raise InputError("Email invalid")
+    # check if the email is registered
+    if not if_email_exist(email):
+        raise InputError("Email does not belong to a user")
+
+    # email belong to a user, get the user from database
+    user = User.get_user_by_email(email)
+    # check if the input password match the user's password
+    # if not mach
+    if user.password != hash_password(passowrd):
+        raise InputError("Password Incorrect")
+    # if password match, generate new token and set the user's state to login
+    else:
+        token = generate_token(user.u_id)
+        user.if_logged_in = True
+        
+        return {'u_id': u_id, 'token': token}
+# given an active token, invalidates the token to log the user out
+# if a vaid token is given, and the user is successfully logged out -> true, otherwise -> false
+def auth_logout(token):
+    user = User.get_user_by_token(token)
+    # if there is to user corresponding to token
+    if user is not None:
+        user.if_logged_in = False
+        user.token = None
+        return {"is_success": True}
+    return {"is_success": False}
 
 
 if __name__ == "__main__":
