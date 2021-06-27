03686adba8d25a17bd7208e7fd9e77465defbaca
KLin00 <47765381+KLin00@users.noreply.github.com>
2021-06-27 16:56:01 +1000

    Updated user_db.py
    added stub functions

--- backend/user_db.py
+++ backend/user_db.py
@@ -40,11 +40,22 @@ class Diner(db.Model):
     email = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(50), nullable=False)
     phone_number = db.Column(db.String(20))
+    
+    def __init__(self, first_name, last_name, email, password, phone_number):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.phone_number = phone_number
 
         
 class Image(db.Model):
     eatery_id = db.Column(db.Integer, db.ForeignKey('eatery.id'), primary_key=True)
     image = db.Column(db.String(50), nullable=False, primary_key=True)
+    
+    def __init__(self, eatery_id, image):
+        self.eatery_id = eatery_id
+        self.image = image
 
 class Voucher(db.Model): 
     id = db.Column(db.Integer, primary_key=True)
@@ -53,6 +64,13 @@ class Voucher(db.Model):
     start_time = db.Column(db.Time, nullable=False)
     end_time = db.Column(db.Time, nullable=False)
     discount = db.Column(db.Float, nullable=False)
+    
+    def __init__(self, eatery_id, date, start_time, end_time, discount):
+        self.eatery_id = eatery_id
+        self.date = date
+        self.start_time = start_time
+        self.end_time = end_time
+        self.discount = discount
 
 class Schedule(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -60,6 +78,12 @@ class Schedule(db.Model):
     no_vouchers = db.Column(db.Integer, nullable=False) 
     weekday = db.Column(db.String(10), nullable=False)   # Mon to Sun
     discount = db.Column(db.Float, nullable=False)
+    
+    def __init__(self, eatery_id, no_vouchers, weekday, discount):
+        self.eatery_id = eatery_id
+        self.no_vouchers = no_vouchers
+        self.weekday = weekday
+        self.discount = discount
 
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -67,6 +91,12 @@ class Review(db.Model):
     eatery_id = db.Column(db.Integer, db.ForeignKey('eatery.id'))
     comment = db.Column(db.String(20))
     rating = db.Column(db.Integer, nullable=False)
+    
+    def __init__(self, diner_id, eatery_id, comment, rating):
+        self.diner_id = diner_id
+        self.eatery_id = eatery_id
+        self.comment = comment
+        self.rating = rating
 
 
 class Booking(db.Model):
@@ -74,8 +104,21 @@ class Booking(db.Model):
     diner_id = db.Column(db.Integer, db.ForeignKey('diner.id'))
     eatery_id = db.Column(db.Integer, db.ForeignKey('eatery.id'))
     if_used = db.Column(db.Boolean)
+    
+    def __init__(self, diner_id, eatery_id, if_used):
+       self.diner_id = diner_id
+       self.eatery_if = eatery_id
+       self.if_used = if_used
 
-
+class Offer(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     schedule_id = db.Column(db.Integer, db.ForeignKey('schedule.id'))
+     voucher_id = db.Column(db.Integer, db.ForeignKey('voucher.id'))
+     
+     def __init__(self, schedule_id, voucher_id):
+       self.schedule_id = schedule_id
+       self.voucher_id = voucher_id
+        
 """
 CREATE DOMAIN Weekdays AS 
    CHECK (VALUE IN ('Mon','Tues','Wed','Thurs','Fri', 'Sat', 'Sun'));
